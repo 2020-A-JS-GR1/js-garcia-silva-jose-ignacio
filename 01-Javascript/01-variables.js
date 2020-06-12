172ddbdd7cc082116b3f80e712efa8df727adb2c
José I. García <burmamachoy@tutanota.com>
2020-06-12 18:38:38 -0400

    truty falsy  0

--- 01-Javascript/01-variables.js
+++ 01-Javascript/01-variables.js
@@ -16,9 +16,49 @@ const texto = "José"; // string
 const booleano = false; // boolean
 const hijos = null; // object
 const zapatos = undefined // undefined
+const apellido = 'Eguez'; // undefined
 console.log(typeof numero);
 console.log(typeof sueldo);
 console.log(typeof texto);
 console.log(typeof booleano);
 console.log(typeof hijos);
 console.log(typeof zapatos);
+console.log(typeof apellido);
+console.log(typeof Number("asd"));
+console.log(Number("asd"));
+
+if(true == true){
+    console.log("Es verdadero");
+}else{
+    console.log("Es falso") //!
+}
+
+if(""){
+    console.log("Es verdadero");
+}else{
+    console.log("Es falso") //!
+}
+
+if("García"){
+    console.log("Es verdadero");
+}else{
+    console.log("Es falso") //!
+}
+
+if(1){
+    console.log("Es verdadero");
+}else{
+    console.log("Es falso") //!
+}
+
+if(0){
+    console.log("Es verdadero");
+}else{
+    console.log("Es falso") //!
+}
+
+if(1){
+    console.log("Es verdadero");
+}else{
+    console.log("Es falso") //!
+}
