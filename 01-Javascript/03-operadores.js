5947ca37ea8183fe234374871b9c06cfe8993bae
José I. García <burmamachoy@tutanota.com>
2020-06-19 18:32:27 -0400

    hasta mamp

---
+++ 01-Javascript/03-operadores.js
@@ -0,0 +1,102 @@
+const arreglo = [
+    {
+        id:1,
+        nombre: 'José',
+        nota:5
+    },
+    {
+        id:2,
+        nombre: 'Ignacio',
+        nota:4
+    },
+    {
+        id:3,
+        nombre: 'Johanna',
+        nota:6
+    },
+    {
+        id:4,
+        nombre: 'Daniel',
+        nota:7
+    },
+    {
+        id:5,
+        nombre: 'Juan',
+        nota:2
+    },
+    {
+        id:6,
+        nombre: 'Vicente',
+        nota:9
+    },
+    {
+        id:7,
+        nombre: 'Iván',
+        nota:17
+    },
+    {
+        id:8,
+        nombre: 'Katherin',
+        nota:6
+    },
+    {
+        id:9,
+        nombre: 'Camilo',
+        nota:13
+    },
+    {
+        id:10,
+        nombre: 'Felipe',
+        nota:14
+    }
+]
+
+// FUNCIONES COMO PARAMETROS
+
+// FIND
+// devolver una expersion -> TRUTY FALSY
+const respuestaFind = arreglo
+.find(
+    function(valorActual, indiceActual, arregloCompleto) {
+        console.log('valorActual', valorActual);
+        console.log('indiceActual', indiceActual);
+        console.log('arregloCompleto', arregloCompleto);
+        return valorActual.nombre === "Vicente";
+    }
+);
+console.log('respuestaFind', respuestaFind); // undefined
+
+const respuestaIndex = arreglo
+    .findIndex(
+        function(valorActual, indiceActual, arregloCompleto) {
+            console.log('valorActual', valorActual);
+            console.log('indiceActual', indiceActual);
+            console.log('arregloCompleto', arregloCompleto);
+            return valorActual.nombre === "Vicente";
+        }
+    );
+console.log('respuestaFind', respuestaIndex); // undefined
+
+// FOREACH
+const respuestaForEach = arreglo
+    .forEach(
+        function(valorActual, indiceActual, arregloCompleto) {
+            console.log('valorActual', valorActual);
+            console.log('indiceActual', indiceActual);
+            console.log('arregloCompleto', arregloCompleto);
+        }
+    );
+console.log('respuestaFind', respuestaForEach); // undefined
+
+// MAP
+// devolver NUEVO ELEMENTO
+const respuestMap = arreglo
+    .map(
+        function(valorActual, indiceActual, arregloCompleot) {
+            valorActual.nota = valorActual.nota + 1;
+            return valorActual;
+        }
+    );
+console.log('respuestaMapNuevo', respuestaMapNuevo);
+console.log('arreglo', arreglo);
+
